58b64d71fa70e9a0ea1aa6c056b46d7faf47ba8e
oneofthezombies <hunhoekim@gmail.com>
2024-03-06 20:05:27 +0900

    chore: base structure

---
+++ kero_log/kero_log.h
@@ -0,0 +1,12 @@
+#ifndef KERO_LOG_H
+#define KERO_LOG_H
+
+namespace kero {
+namespace log {
+
+//
+
+} // namespace log
+} // namespace kero
+
+#endif // KERO_LOG_H
---
+++ kero_log/kero_log_test.cc
@@ -0,0 +1,4 @@
+#include "kero_log.h"
+#include <gtest/gtest.h>
+
+TEST(LogTest, Base) { ASSERT_EQ(1, 1); }
